1720a8a843207be236d67a588fd61cefcb4f2c02
Igwilo Ethel-Doris <etheldorisamarachi@gmail.com>
2024-02-04 22:14:39 +0100

    updated

--- src/Myform.jsx
+++ src/Myform.jsx
@@ -5,20 +5,24 @@ const Myform = () => {
     <div>
       <form action="">
         <div>
-            <label htmlFor="username">Username</label>
+            <label htmlFor="username">Username</label><br />
             <input id='username' type="text" className="username" />
         </div>
         <div>
-            <label htmlFor="email">Email</label>
+            <label htmlFor="email">Email</label><br />
             <input id='email' type="email" className="email" />
         </div>
         <div>
-            <label htmlFor="password">Password</label>
+            <label htmlFor="password">Password</label><br />
             <input id='password' type="text" className="password" />
         </div>
         <div>
-            <label htmlFor="confirmpassword">Confirm Password</label>
+            <label htmlFor="confirmpassword">Confirm Password</label><br />
             <input id='confirmpassword' type="text"  />
+        </div>
+        <div>
+            <button>Claim your free trial</button>
+            <p>By clicking this button, you are agreeing to our<span>Terms and Conditions</span></p>
         </div>
       </form>
     </div>
